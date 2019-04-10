0e176bcc72c1b36fd052f456b3672c37151525f9
Ann Wang <xiaoxiaogudg@163.com>
2019-04-10 12:33:45 -0700

    process.env.MicrosoftAppId

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ var builder = require('botbuilder');
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url);
-});
+// server.listen(process.env.port || process.env.PORT || 3978, function () {
+//    console.log('%s listening to %s', server.name, server.url);
+// });
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
