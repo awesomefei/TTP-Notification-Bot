670b0ce03718b84441de3cfa1143598ad1ff1faf
Ann Wang <xiaoxiaogudg@163.com>
2019-04-11 10:27:15 -0700

    installed bot teams

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ A simple echo bot for the Microsoft Bot Framework.
 
 // var restify = require('restify');
 var builder = require('botbuilder');
+var teams =require('botbuilder-teams');
 // var azure = require('azure-storage');
 // var botbuilder_azure = require("botbuilder-azure");
 
@@ -66,10 +67,24 @@ var address =
 }
 console.log('sending*********!!!!!*****finished');
 
+bot.dialog('NotificationFeed', function (session) {
+    // user name/user id
+    var msg = new teams.TeamsMessage(session).text("This is a test notification message.");
+    // This is a dictionary which could be merged with other properties
+    var alertFlag = teams.TeamsMessage.alertFlag;
+    var notification = (msg).sourceEvent({
+      '*' : alertFlag
+    });
+  
+    // this should trigger an alert
+    session.send(notification);
+    session.endDialog();
+  });
+  
 var msg = new builder.Message().address(address);
 msg.text('Hello, this is a notification');
 msg.summary('This is  a summary');
-msg.sourceEvent ({ notification: {alert: true }});
+msg.sourceEvent ({ notification: { alert: true }});
 bot.send(msg);
 
 // Intercept trigger event (ActivityTypes.Trigger)
